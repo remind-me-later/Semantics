a387471d152816dcfb2e09fe235470f5f6b8e979
Ricardo Maurizio Paul <memaurifu14@hotmail.com>
2022-02-16 16:22:33 +0100

    better model

--- imp.v
+++ imp.v
@@ -22,20 +22,22 @@ Qed.
 Theorem eqb_string_true_iff : forall x y : string,
   eqb_string x y = true <-> x = y.
 Proof.
-  intros x y.
+  intros.
   unfold eqb_string.
   destruct (string_dec x y) as [Hs_eq | Hs_not_eq].
   - rewrite Hs_eq. split. reflexivity. reflexivity.
   - split.
-    + intros contra. discriminate contra.
-    + intros H. exfalso. apply Hs_not_eq. apply H.
+    + intros. discriminate.
+    + intros. exfalso. apply Hs_not_eq. apply H.
 Qed.
 
 Theorem eqb_string_false_iff : forall x y : string,
   eqb_string x y = false <-> x <> y.
 Proof.
-  intros x y. rewrite <- eqb_string_true_iff.
-  rewrite not_true_iff_false. reflexivity. 
+  intros. 
+  rewrite <- eqb_string_true_iff.
+  rewrite not_true_iff_false.
+  reflexivity. 
 Qed.
 
 Definition total_map (A : Type) := string -> A.
@@ -184,35 +186,43 @@ Inductive bevalR : state -> bexp -> bool -> Prop :=
 Theorem aeval_iff_aevalR : forall a n st,
   (aevalR st a n) <-> aeval st a = n.
 Proof.
-  split.
-  - intros H. induction H; subst; reflexivity.
+  intros; split.
+
+  - intros.
+    induction H.
+    3, 4, 5: subst.
+    all: reflexivity.
+
   - generalize dependent n.
-    induction a; simpl; intros; subst; constructor;
-    try apply IHa1; try apply IHa2; reflexivity.
+    induction a; simpl.
+    all: intros; subst; constructor.
+    1, 3, 5: apply IHa1.
+    4, 5, 6: apply IHa2.
+    all: reflexivity.
 Qed.
 
 Theorem beval_iff_bevalR : 
   forall e b st, (bevalR st e b) <-> beval st e = b.
 Proof.
-  split.
-  - intros H. induction H; subst; 
-    try reflexivity;
-    unfold beval;
-    apply (aeval_iff_aevalR e1 n1) in H1;
-    apply (aeval_iff_aevalR e2 n2) in H2;
-    induction H1; 
-    induction H2; 
-    reflexivity.
+  intros; split.
+
+  - intros; induction H.
+    1, 2: reflexivity.
+    3, 4: subst; reflexivity.
+    all: unfold beval.
+    all: 
+      apply (aeval_iff_aevalR e1 n1) in H1;
+      apply (aeval_iff_aevalR e2 n2) in H2.
+    all: induction H1; induction H2; reflexivity.
   
-  - generalize dependent b; 
-    induction e; 
-    simpl; intros; subst; unfold beval; 
-    constructor; 
-    try apply aeval_iff_aevalR;
-    try apply IHe; 
-    try apply IHe1; 
-    try apply IHe2; 
-    reflexivity.
+  - generalize dependent b.
+    induction e; simpl; intros; subst. 
+    1, 2: constructor.
+    1, 2: constructor; apply aeval_iff_aevalR; reflexivity.
+    1: constructor. apply IHe. reflexivity. 
+    1: constructor.
+    apply IHe1. reflexivity.
+    apply IHe2. reflexivity.
 Qed.
 
 (* Commands *)
@@ -251,8 +261,9 @@ Reserved Notation
 
 (* command evaluation *)
 Inductive ceval : com -> state -> state -> Prop :=
-  | E_Skip : forall st,
-      [ skip, st ] => st
+  | E_Skip : forall st st',
+      st = st' ->
+      [ skip, st ] => st'
   | E_Asgn : forall st a n x,
       aeval st a = n ->
       [ x := a, st ] => (x !-> n ; st)
@@ -260,30 +271,25 @@ Inductive ceval : com -> state -> state -> Prop :=
       [ c1, st ] => st'' ->
       [ c2, st'' ] => st' ->
       [ c1 ; c2, st ] => st'
-  | E_IfTrue : forall st st' b c1 c2,
-      beval st b = true ->
-      [ c1, st ] => st' ->
-      [ if b then c1 else c2 end, st] => st'
-  | E_IfFalse : forall st st' b c1 c2,
-      beval st b = false ->
+  | E_If : forall st st' b c1 c2,
+      beval st b = true /\
+      [ c1, st ] => st' \/
+      beval st b = false /\
       [ c2, st ] => st' ->
-      [ if b then c1 else c2 end, st ]=> st'
-  | E_WhileFalse : forall b st c,
-      beval st b = false ->
-      [ while b do c end, st ] => st
-  | E_WhileTrue : forall st st' st'' b c,
-      beval st b = true ->
-      [ c, st ] => st'' ->
-      [ while b do c end, st'' ] => st' ->
+      [ if b then c1 else c2 end, st] => st'
+  | E_While : forall st st' st'' b c,
+      beval st b = true /\
+      [ c, st ] => st'' /\
+      [ while b do c end, st'' ] => st' \/
+      beval st b = false /\
+      st = st' ->
       [ while b do c end, st ] => st'
-  | E_RepeatFalse : forall st st' b c,
-      beval st b = false ->
+  | E_Repeat : forall st st' st'' b c,
+      beval st b = true /\
+      [ c, st ] => st'' /\
+      [ repeat c until b end, st'' ] => st' \/
+      beval st b = false /\
       [ c, st ] => st' ->
-      [ repeat c until b end, st ] => st'
-  | E_RepeatTrue : forall st st' st'' b c,
-      beval st b = true ->
-      [ c, st ] => st'' ->
-      [ repeat c until b end, st'' ] => st' ->
       [ repeat c until b end, st ] => st'
   where "[ c , st ] => st'" := (ceval c st st').
 
@@ -297,27 +303,43 @@ Example ceval_example1:
   (z !-> 4 ; x !-> 2).
 Proof.
   apply E_Seq with (x !-> 2).
-  - apply E_Asgn. reflexivity.
-  - apply E_IfFalse.
+  - apply E_Asgn. 
     reflexivity.
-    apply E_Asgn. reflexivity.
+
+  - apply E_If.
+    simpl.
+    right. split. reflexivity.
+    apply E_Asgn.
+    reflexivity.
 Qed.
 
-
+Lemma if_skip_skip_equiv_skip : forall b st,
+  [if b then skip else skip end, st] => st.
+Proof.
+  intros. apply E_If. induction beval.
+  1: left; split.
+  3: right; split. 
+  1, 3: reflexivity.
+  all: constructor; reflexivity.
+Qed.
 
 (* exercise 2.7 *)
 Lemma while_equiv_repeat : forall b c st st',
-  [ repeat c until b end, st ] => st' <-> 
-  [ if b then skip else repeat c until b end end, st ] => st'.
+  let r := <{repeat c until b end}> in
+  [ r, st ] => st' <-> 
+  [ c; if b then skip else r end, st ] => st'.
 Proof.
   split.
-  - intros. induction H.
-    + 
+  - intros. 
+    induction H.
+    + apply E_Seq with st'. induction H. constructor.
+
 
 (* proposition 2.8 *)
 Lemma while_equiv_if : forall b c st st',
   let w := <{while b do c end}> in
   [w, st] => st' <-> [ if b then c; w else skip end, st] => st'.
 Proof.
-  split.
-  - intros. induction H. induction CSkip.
+  intros.
+  induction w0.
+  - split; intros. constructor.
