c277434b5e5ca007fa754ef0913b53d63ffaa668
Ricardo Maurizio Paul <memaurifu14@hotmail.com>
2022-02-28 11:52:57 +0100

    fail more

--- small_step.v
+++ small_step.v
@@ -10,62 +10,57 @@ From Coq Require Import Lia.
 Inductive aval : aexp -> Prop :=
   | av_num : forall n, aval (ANum n).
 
-Reserved Notation " a '/' st 'a.>' a' "
-                  (at level 40, st at level 39).
-
 Inductive astep (st : state) : aexp -> aexp -> Prop :=
   | AS_Id : forall (i : string),
-      i / st a.> (st i)
+      astep st i (st i)
   | AS_Plus1 : forall a1 a1' a2,
-      a1 / st a.> a1' ->
-      <{ a1 + a2 }> / st a.> <{ a1' + a2 }>
+      astep st a1 a1' ->
+      astep st <{ a1 + a2 }> <{ a1' + a2 }>
   | AS_Plus2 : forall v1 a2 a2',
       aval v1 ->
-      a2 / st a.> a2' ->
-      <{ v1 + a2 }> / st a.> <{ v1 + a2' }>
+      astep st a2 a2' ->
+      astep st <{ v1 + a2 }> <{ v1 + a2' }>
   | AS_Plus : forall (n1 n2 : nat),
-      <{ n1 + n2 }> / st a.> (n1 + n2)
+      astep st <{ n1 + n2 }> (n1 + n2)
   | AS_Minus1 : forall a1 a1' a2,
-      a1 / st a.> a1' ->
-      <{ a1 - a2 }> / st a.> <{ a1' - a2 }>
+      astep st a1 a1' ->
+      astep st <{ a1 - a2 }> <{ a1' - a2 }>
   | AS_Minus2 : forall v1 a2 a2',
       aval v1 ->
-      a2 / st a.> a2' ->
-      <{ v1 - a2 }> / st a.> <{ v1 - a2' }>
+      astep st a2 a2' ->
+      astep st <{ v1 - a2 }> <{ v1 - a2' }>
   | AS_Minus : forall (n1 n2 : nat),
-      <{ n1 - n2 }> / st a.> (n1 - n2)
+      astep st <{ n1 - n2 }> (n1 - n2)
   | AS_Mult1 : forall a1 a1' a2,
-      a1 / st a.> a1' ->
-      <{ a1 * a2 }> / st a.> <{ a1' * a2 }>
+      astep st a1 a1' ->
+      astep st <{ a1 * a2 }> <{ a1' * a2 }>
   | AS_Mult2 : forall v1 a2 a2',
       aval v1 ->
-      a2 / st a.> a2' ->
-      <{ v1 * a2 }> / st a.> <{ v1 * a2' }>
+      astep st a2 a2' ->
+      astep st <{ v1 * a2 }> <{ v1 * a2' }>
   | AS_Mult : forall (n1 n2 : nat),
-      <{ n1 * n2 }> / st a.> (n1 * n2)
-
-    where " a '/' st 'a.>' a' " := (astep st a a').
+      astep st <{ n1 * n2 }> (n1 * n2).
 
 Reserved Notation " b '/' st 'b.>' b' "
                   (at level 40, st at level 39).
 
 Inductive bstep (st : state) : bexp -> bexp -> Prop :=
 | BS_Eq1 : forall a1 a1' a2,
-    a1 / st a.> a1' ->
+    astep st a1 a1' ->
     <{ a1 = a2 }> / st b.> <{ a1' = a2 }>
 | BS_Eq2 : forall v1 a2 a2',
     aval v1 ->
-    a2 / st a.> a2' ->
+    astep st a2 a2' ->
     <{ v1 = a2 }> / st b.> <{ v1 = a2' }>
 | BS_Eq : forall (n1 n2 : nat),
     <{ n1 = n2 }> / st b.>
     (if (n1 =? n2) then <{ true }> else <{ false }>)
 | BS_LtEq1 : forall a1 a1' a2,
-    a1 / st a.> a1' ->
+    astep st a1 a1' ->
     <{ a1 <= a2 }> / st b.> <{ a1' <= a2 }>
 | BS_LtEq2 : forall v1 a2 a2',
     aval v1 ->
-    a2 / st a.> a2' ->
+    astep st a2 a2' ->
     <{ v1 <= a2 }> / st b.> <{ v1 <= a2' }>
 | BS_LtEq : forall (n1 n2 : nat),
     <{ n1 <= n2 }> / st b.>
@@ -93,32 +88,34 @@ Reserved Notation
   (at level 39, c custom com at level 99, c1 custom com at level 99,
     st constr, st1 constr at next level).
 
-Inductive cstep : (com * state) -> (com * state) -> Prop :=
-| CS_AsgnStep : forall st i a1 a1',
-  a1 / st a.> a1' ->
+Inductive small_step : (com * state) -> (com * state) -> Prop :=
+| SS_Skip : forall st,
+    [skip, st] .> [skip, st]
+| SS_AsgnStep : forall st i a1 a1',
+  astep st a1 a1' ->
   [i := a1, st] .> [i := a1', st]
-| CS_Asgn : forall st i (n : nat),
+| SS_Asgn : forall st i (n : nat),
   [i := n, st] .> [skip, (i !-> n ; st)]
-| CS_SeqStep : forall st c1 c1' st' c2,
+| SS_SeqStep : forall st c1 c1' st' c2,
   [c1, st] .> [c1', st'] ->
   [c1; c2, st] .> [c1'; c2, st']
-| CS_SeqFinish : forall st c2,
+| SS_SeqFinish : forall st c2,
   [skip; c2, st] .> [c2, st]
-| CS_IfStep : forall st b1 b1' c1 c2,
+| SS_IfStep : forall st b1 b1' c1 c2,
   b1 / st b.> b1' ->
   [if b1 then c1 else c2 end, st]
   .>
   [if b1' then c1 else c2 end, st]
-| CS_IfTrue : forall st c1 c2,
+| SS_IfTrue : forall st c1 c2,
   [if true then c1 else c2 end, st] .> [c1, st]
-| CS_IfFalse : forall st c1 c2,
+| SS_IfFalse : forall st c1 c2,
   [if false then c1 else c2 end, st] .> [c2, st]
-| CS_While : forall st b1 c1,
+| SS_While : forall st b1 c1,
   [while b1 do c1 end, st]
   .>
   [if b1 then c1; while b1 do c1 end else skip end, st]
 where "'[' c ',' st ']' '.>' '[' c1 ',' st1 ']'"
-  := (cstep (c,st) (c1,st1)).
+  := (small_step (c,st) (c1,st1)).
 
 Definition relation (X : Type) := X -> X -> Prop.
 Inductive multi {X : Type} (R : relation X) : relation X :=
@@ -128,7 +125,81 @@ Inductive multi {X : Type} (R : relation X) : relation X :=
                   multi R y z ->
                   multi R x z.
 
+Theorem multi_R : forall (X : Type) (R : relation X) (x y : X),
+    R x y -> (multi R) x y.
+Proof.
+  intros X R x y H.
+  apply multi_step with y. apply H. apply multi_refl.
+Qed.
+
+Theorem multi_trans : forall (X : Type) (R : relation X) (x y z : X),
+      multi R x y ->
+      multi R y z ->
+      multi R x z.
+Proof.
+  intros X R x y z G H.
+  induction G.
+    - (* multi_refl *) assumption.
+    - (* multi_step *)
+      apply multi_step with y. assumption.
+      apply IHG. assumption.
+Qed.
+
+Theorem astep_deterministic: forall st a1 a2 a3,
+    astep st a1 a2 ->
+    astep st a1 a3 ->
+    a2 = a3.
+Proof.
+Admitted.
+
+
+Definition deterministic {X: Type} (R: relation X) :=
+  forall x y1 y2 : X, R x y1 -> R x y2 -> y1 = y2. 
+
+Theorem step_deterministic:
+  deterministic small_step.
+Proof.
+  unfold deterministic. intros x y1 y2 Hy1 Hy2.
+  generalize dependent y2.
+  induction Hy1.
+  - intros. inversion Hy2; subst. reflexivity.
+  - intros. inversion Hy2; subst.
+    + assert (a1' = a1'0). 
+      apply astep_deterministic with st a1.
+      assumption. assumption.
+      rewrite H0. reflexivity.
+    + inversion H.
+  - intros. inversion Hy2; subst.
+    + inversion H3.
+    + reflexivity.
+Admitted.
+
 Notation "'[' c ','  st ']' '*>' '[' c1 ',' st1 ']'" :=
-  (multi cstep (c,st) (c1, st1))
+  (multi small_step (c,st) (c1, st1))
   (at level 40, c custom com at level 99, c1 custom com at level 99,
     st constr, st1 constr at next level).
+
+Theorem eq_final : forall st st1,
+    [skip, st] *> [skip, st1] ->
+    st = st1.
+Admitted.
+
+(* Lemma 2.19 *)
+Theorem decons_seq : forall c1 c2 st st2 st1, 
+    [c1; c2, st] *> [skip, st2] ->
+    [c1, st] *> [skip, st1] ->
+    [c2, st1] *> [skip, st2].
+Proof.
+    intros.
+    induction c1.
+    apply eq_final in H0.
+    rewrite <- H0.
+    inversion H; subst.
+    assert (small_step (<{skip; c2}>, st1) (<{c2}>, st1)).
+    apply SS_SeqFinish.
+    apply (multi_step small_step (<{skip; skip; c2}>, st1)) in H.
+    assumption.
+    admit.
+    rewrite H0 in H1.
+Qed.
+    
